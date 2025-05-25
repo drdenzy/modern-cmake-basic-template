15c93781d63c62302ef24e4d3ad26a30406dfe70
Dennis Akpenyi <dennisakpenyi@gmail.com>
2025-05-25 20:26:41 +0200

    fixed clang-tidy issue

--- cmake/Tools.cmake
+++ cmake/Tools.cmake
@@ -75,6 +75,51 @@ function(add_clang_format_target)
     endif()
 endfunction()
 
+# function(add_clang_tidy_to_target target)
+#     get_target_property(TARGET_SOURCES ${target} SOURCES)
+#     list(
+#         FILTER
+#         TARGET_SOURCES
+#         INCLUDE
+#         REGEX
+#         ".*.(cc|h|cpp|hpp)")
+
+#     find_package(Python3 COMPONENTS Interpreter)
+#     if(NOT ${Python_FOUND})
+#         message(WARNING "Python3 needed for Clang-Tidy")
+#         return()
+#     endif()
+
+#     find_program(CLANGTIDY clang-tidy)
+#     if(CLANGTIDY)
+#         if(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+#             message(STATUS "Added MSVC ClangTidy (VS GUI only) for: ${target}")
+#             set_target_properties(
+#                 ${target} PROPERTIES VS_GLOBAL_EnableMicrosoftCodeAnalysis
+#                                      false)
+#             set_target_properties(
+#                 ${target} PROPERTIES VS_GLOBAL_EnableClangTidyCodeAnalysis true)
+#         else()
+#             message(STATUS "Added Clang Tidy for Target: ${target}")
+#             add_custom_target(
+#                 ${target}_clangtidy
+#                 COMMAND
+#                     ${Python3_EXECUTABLE}
+#                     ${CMAKE_SOURCE_DIR}/tools/run-clang-tidy.py
+#                     ${TARGET_SOURCES}
+#                     -config-file=${CMAKE_SOURCE_DIR}/.clang-tidy
+#                     -extra-arg-before=-std=${CMAKE_CXX_STANDARD}
+#                     -header-filter="\(src|app\)\/*.\(h|hpp\)"
+#                     -p=${CMAKE_BINARY_DIR}
+#                 WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+#                 USES_TERMINAL)
+#         endif()
+#     else()
+#         message(WARNING "CLANGTIDY NOT FOUND")
+#     endif()
+# endfunction()
+
+
 function(add_clang_tidy_to_target target)
     get_target_property(TARGET_SOURCES ${target} SOURCES)
     list(
@@ -82,7 +127,7 @@ function(add_clang_tidy_to_target target)
         TARGET_SOURCES
         INCLUDE
         REGEX
-        ".*.(cc|h|cpp|hpp)")
+        ".*\\.(cc|h|cpp|hpp)")
 
     find_package(Python3 COMPONENTS Interpreter)
     if(NOT ${Python_FOUND})
@@ -96,20 +141,20 @@ function(add_clang_tidy_to_target target)
             message(STATUS "Added MSVC ClangTidy (VS GUI only) for: ${target}")
             set_target_properties(
                 ${target} PROPERTIES VS_GLOBAL_EnableMicrosoftCodeAnalysis
-                                     false)
+                                         false)
             set_target_properties(
                 ${target} PROPERTIES VS_GLOBAL_EnableClangTidyCodeAnalysis true)
         else()
             message(STATUS "Added Clang Tidy for Target: ${target}")
             add_custom_target(
-                ${target}_clangtidy
+                ${target}_clangtidy_${CMAKE_PROJECT_NAME}
                 COMMAND
                     ${Python3_EXECUTABLE}
                     ${CMAKE_SOURCE_DIR}/tools/run-clang-tidy.py
                     ${TARGET_SOURCES}
                     -config-file=${CMAKE_SOURCE_DIR}/.clang-tidy
                     -extra-arg-before=-std=${CMAKE_CXX_STANDARD}
-                    -header-filter="\(src|app\)\/*.\(h|hpp\)"
+                    -header-filter="\\(src|app\\)\\/*\\.(h|hpp)"
                     -p=${CMAKE_BINARY_DIR}
                 WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
                 USES_TERMINAL)
